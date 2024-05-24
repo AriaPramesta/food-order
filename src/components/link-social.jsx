31e21b86d2b9520aaf0cd2eb50dc8d93506b7d6f
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-05-24 18:20:58 +0700

    feat: edit welcome page style

--- src/components/link-social.jsx
+++ src/components/link-social.jsx
@@ -1,6 +1,6 @@
 const LinkSocial = ({ urlImg, children }) => {
   return (
-    <button className="bg-white rounded-full px-4 py-2 flex items-center gap-3">
+    <button className="bg-white rounded-full px-4 py-2 flex gap-3 items-center font-semibold text-lg hover:shadow-2xl">
       <img src={urlImg} width={20} />
       {children}
     </button>
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -3,54 +3,54 @@ import Button from "../components/button";
 
 export function Welcome() {
   return (
-    <div className="max-w-[375px] min-h-screen px-9 pt-8 bg-[url('/assets/welcome-bg.png')] m-auto shadow-2xl relative text-xs font-plusJakarta">
-      <a
-        href="/"
-        className="w-[55px] h-[32px] text-xs px-3 py-2 font-semibold rounded-full shadow-xl shadow-black text-[#FE724C] ml-[270px] static bg-white "
-      >
-        Skip
-      </a>
-
-      <div className="w-[311px] h-[184px] static mt-28 mb-[213px]">
-        <h1 className="text-5xl font-bold mb-5">
-          Welcome to <p className="text-[#FE724C]">FoodHub</p>
-        </h1>
-        <h3 className=" text-lg">
-          Your favourite foods delivered <br /> fast at your door.
-        </h3>
-      </div>
-
-      <div className="flex items-center mb-[18px]">
-        <div className="flex-1">
-          <div className="h-[1px] border-b border-black"></div>
-        </div>
-        <p className="px-2">sign in with</p>
-        <div className="flex-1">
-          <div className="h-[1px] border-b border-black"></div>
-        </div>
-      </div>
-
-      <div className="mb-[23px] flex gap-2 justify-between">
-        <LinkSocial urlImg="/assets/facebook.png">Facebook</LinkSocial>
-        <LinkSocial urlImg="/assets/google.png">Google</LinkSocial>
-      </div>
-
-      <div>
-        <Button theme="secondary" isFull>
-          Start with email or phone
-        </Button>
-      </div>
-
-      {/* <button className=" outline outline-1 outline-white text-white px-5 py-3 rounded-full w-full bg-slate-400 mb-6 font-semibold">
-        Start with email or phone
-      </button> */}
-
-      <p className="text-center text-sm">
-        Already have account?
-        <a href="/" className="underline">
-          Sign in
+    <div className="max-w-[375px] min-h-screen px-9 pt-8 bg-[url('/assets/welcome-bg.png')] m-auto shadow-2xl relative text-xs font-plusJakarta bg-cover flex flex-col">
+      <div className="flex-1">
+        <a
+          href="/"
+          className="w-[55px] h-[32px] text-xs px-3 py-2 font-semibold rounded-full shadow-xl shadow-black text-[#FE724C] ml-[270px] static bg-white "
+        >
+          Skip
         </a>
-      </p>
+
+        <div className="w-[311px] h-[184px] static mt-28">
+          <h1 className="text-5xl font-bold mb-5">
+            Welcome to <p className="text-[#FE724C]">FoodHub</p>
+          </h1>
+          <h3 className=" text-lg">
+            Your favourite foods delivered <br /> fast at your door.
+          </h3>
+        </div>
+      </div>
+
+      <div className="mb-10">
+        <div className="flex items-center">
+          <div className="flex-1">
+            <div className="h-[1px] border-b border-black"></div>
+          </div>
+          <p className="px-2">sign in with</p>
+          <div className="flex-1">
+            <div className="h-[1px] border-b border-black"></div>
+          </div>
+        </div>
+
+        <div className=" flex gap-2 justify-between mt-10">
+          <LinkSocial urlImg="/assets/facebook.png">Facebook</LinkSocial>
+          <LinkSocial urlImg="/assets/google.png">Google</LinkSocial>
+        </div>
+
+        <div className="mt-10">
+          <Button theme="secondary" isFull>
+            Start with email or phone
+          </Button>
+        </div>
+
+        <p className="text-center text-sm mt-10">
+          Already have account?
+          <a href="/" className="underline">
+            Sign in
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
